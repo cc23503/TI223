b451ba48345a7ead57402ff2665dcf2a89744a00
u23503 <cc23503@g.unicamp.br>
2025-08-26 16:05:36 -0300

    feat: experimentando/comparando API'S dos tipos GET e POST (consulta e inserção no BD)

--- exemplo_api/index.js
+++ exemplo_api/index.js
@@ -13,14 +13,12 @@ app.use(express.json());
 async function conectaBD() {
     try {
         await mssql.connect(stringSQL); // aguarda o retorno do BD
-        const result = await mssql.query("SELECT * FROM dbo.aula_aluno");
-        await mssql.close(); // Fecha a conexão depois de usar
-        return result.recordset;
     } catch (error) {
         console.error("Erro ao conectar ou consultar o BD:", error);
         return { erro: "Erro ao acessar o banco de dados" };
     }
 }
+conectaBD();
 
 
 // rota principal
@@ -28,10 +26,38 @@ app.get('/', (req, res) => res.json(
     {mensagem: 'Servidor em execução'}
 ));
 
+
 // rota para buscar dados
-app.get('/dados', async (req, res) => {
-    const dados = await conectaBD(); // aguarda o retorno do BD
-    res.json(dados);
+app.get('/alunos', async (req, res) => {
+    const alunos = await mssql.query("SELECT * FROM dbo.aula_aluno");
+
+    res.json(alunos);
+    console.log(alunos);
+});
+
+
+app.get('/alunos_SP', async (req, res) => {
+    const alunos = await mssql.query("SELECT * FROM dbo.aula_aluno WHERE ufAluno = 'SP'")
+
+    res.json(alunos);
+    console.log(alunos);
+
+});
+
+
+app.post('/alunos', (req, res) => {
+    try {
+        const idAluno = req.body.idAluno;
+        const ra = req.body.ra;
+        const nomeAluno = req.body.nomeAluno;
+        const ufAluno = req.body.ufAluno;
+
+        mssql.query(`INSERT INTO dbo.aula_aluno (idAluno, ra, nomeAluno, ufAluno) VALUES (${idAluno}, ${ra}, '${nomeAluno}', '${ufAluno}')`)
+        res.status(201).json({ "mensagem": "Dados inseridos com sucesso."})
+
+    } catch (erro){
+        console.log("Erro na inserção de dados.", erro)
+    }
 });
 
 // iniciar servidor
